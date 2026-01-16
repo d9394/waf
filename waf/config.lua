751d95c58415c37ce09a1cd9bcb13573f2ea227c
d9394 <15172911+d9394@users.noreply.github.com>
2026-01-16 16:30:49 +0800

    Update log directory and redirect URL in config

--- waf/config.lua
+++ waf/config.lua
@@ -3,7 +3,7 @@
 --waf status
 config_waf_enable = "on"
 --log dir
-config_log_dir = "/tmp"
+config_log_dir = "/var/log/nginx"
 --rule setting
 config_rule_dir = "/usr/local/openresty/nginx/conf/waf/rule-config"
 --enable/disable white url
@@ -23,23 +23,25 @@ config_cookie_check = "on"
 --enable/disable cc filtering
 config_cc_check = "on"
 --cc rate the xxx of xxx seconds
-config_cc_rate = "10/60"
+config_cc_rate = "60/60"
 --enable/disable post filtering
 config_post_check = "on"
+--enable/disable refever filtering
+config_referer_check = "on"
 --config waf output redirect/html
 config_waf_output = "html"
 --if config_waf_output ,setting url
-config_waf_redirect_url = "https://www.unixhot.com"
+config_waf_redirect_url = "https://www.test.net"
 config_output_html=[[
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="Content-Language" content="zh-cn" />
-<title>OpsAny｜Web应用防火墙</title>
+<meta http-equiv="Refresh" content="0;url=https://www.test.net" />
+<title>hellocq｜Web应用防火墙</title>
 </head>
 <body>
-<h1 align="center"> 欢迎白帽子进行授权安全测试，安全漏洞请联系QQ：57459267
+<h1 align="center"> bye
 </body>
 </html>
 ]]
-
