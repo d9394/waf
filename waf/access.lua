6ada6ba902499d8696ec7514ff0f14c6401310a7
d9394 <15172911+d9394@users.noreply.github.com>
2026-01-16 16:29:20 +0800

    Add IP blocking functionality in access.lua

--- waf/access.lua
+++ waf/access.lua
@@ -1,6 +1,20 @@
 require 'init'
 
+function check_and_block()
+    local blacklist = ngx.shared.cc_blacklist
+    local ip = ngx.var.remote_addr
+    if ip == "127.0.0.1" or ip == "::1" then
+        return
+    end
+    local is_banned, err = blacklist:get(ip)
+    if is_banned then
+        ngx.log(ngx.WARN, "Blocked globally by cc_blacklist: ", ip)
+        return ngx.exit(ngx.HTTP_CLOSE)
+    end
+end
+
 function waf_main()
+    check_and_block()
     if white_ip_check() then
     elseif black_ip_check() then
     elseif user_agent_attack_check() then
@@ -9,6 +23,7 @@ function waf_main()
     elseif white_url_check() then
     elseif url_attack_check() then
     elseif url_args_attack_check() then
+    elseif referer_attack_check() then
     --elseif post_attack_check() then
     else
         return
@@ -16,4 +31,3 @@ function waf_main()
 end
 
 waf_main()
-
